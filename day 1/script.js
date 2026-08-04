024aa208b52376b8c6a68c4fa1e4d9bce03e9c23
kunjans2020 <kunjansingh.edu@gmail.com>
2026-08-04 11:35:20 +0530

    Swapping(With/Without)

---
+++ day 1/script.js
@@ -0,0 +1,34 @@
+// Swap using third variable
+
+let a = 20;
+let b = 30;
+let c;
+
+console.log("Before swapping:")
+console.log("a =", a);
+console.log("b =", b);
+
+c = a;
+a = b;
+b = c;
+
+console.log("After swapping:")
+console.log("a =", a);
+console.log("b =", b);
+
+// Swap without using third variable
+
+let x = 50;
+let y = 100;
+
+console.log("Before swapping:")
+console.log("x =", x);
+console.log("y =", y);
+
+x = x + y; // x now becomes 150
+y = x - y; // y becomes 50
+x = x - y; // x becomes 100 
+
+console.log("After swapping:")
+console.log("x =", x);
+console.log("y =", y);
